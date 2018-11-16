aab8eca453ad926ac9e918b985aad9e2125e6ed6
chenzio <chenzhihao.chen@huawei.com>
2018-11-16 23:03:09 +0800

    bind routerLinks

--- src/app/left-menu/left-menu.component.ts
+++ src/app/left-menu/left-menu.component.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './left-menu.component.html',
   styleUrls: ['./left-menu.component.css']
 })
-export class LeftMenuComponent { }
+export class LeftMenuComponent {
+  model = {
+    title: 'A Demo',
+    width: 200,
+    items: [{
+      label: 'Dashboard',
+      iconType: 'dashboard',
+      router: ['dashboard']
+    },{
+      label: 'Photos',
+      iconType: 'laptop',
+      router: ['photos']
+    },{
+      label: 'other',
+      iconType: 'user',
+      router: ['other']
+    }]
+
+  }
+}
